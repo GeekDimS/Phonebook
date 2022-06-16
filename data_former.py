8310291e901967f772adb6bfe7346fb06c3df3ba
Julia Kravchenko <js.kravchenko@gmail.com>
2022-06-16 16:30:11 +0200

    Some modules have been updated.

--- data_former.py
+++ data_former.py
@@ -1,1 +1,12 @@
 
+def format_string(number, data_list):
+    length_number = 5
+    length_item = 20
+    result = str(number) + " " * (length_number - len(str(number)))
+    for item in data_list:
+        result += item + " " * (length_item - len(str(item)))
+    return result
+
+
+def from_dict_to_value_list(dict):
+    return [dict['Lastname'], dict['Name'], dict['Phone']]
